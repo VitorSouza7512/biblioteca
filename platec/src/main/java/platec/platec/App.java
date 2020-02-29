7d3d8806032e38ba8d713ec9d865e6bdbe50e4a8
VitorSouza7512 <vitorsouza7512@gmail.com>
2020-02-29 11:15:02 -0300

    Inicio do Projeto

---
+++ platec/src/main/java/platec/platec/App.java
@@ -0,0 +1,13 @@
+package platec.platec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
